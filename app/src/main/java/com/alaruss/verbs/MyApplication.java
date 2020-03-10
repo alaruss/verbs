5bdd3ce2060dbacdd1900494c7b8f43dd4bde515
Nikita Larin <alaruss@gmail.com>
2020-03-10 10:00:53 +0100

    Migrate to Firebase

--- app/src/main/java/com/alaruss/verbs/MyApplication.java
+++ app/src/main/java/com/alaruss/verbs/MyApplication.java
@@ -4,9 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.alaruss.verbs.db.DBHelper;
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
 
 public class MyApplication extends Application {
     private static final String LOG_TAG = MyApplication.class.getSimpleName();
@@ -17,9 +14,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Log.d(LOG_TAG, "App.create");
-        if (BuildConfig.enableCrashlytics) {
-            Fabric.with(this, new Crashlytics());
-        }
     }
 
     public DBHelper getDBHelper() {
